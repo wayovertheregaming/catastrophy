7a80bbefb33e28ba401e29ff32be8dc3481beada
Ben Cragg <bcvery1@gmail.com>
2018-12-01 05:20:20 +0000

    Setting package name to assets for bindata

--- run.sh
+++ run.sh
@@ -5,10 +5,10 @@ go get -v github.com/kevinburke/go-bindata
 go install github.com/kevinburke/go-bindata
 
 echo "Building assets"
-go-bindata -o binassets.go -debug assets/
+go-bindata -o assets/binassets.go -pkg assets -debug assets/...
 
 echo "Running tests"
 go test ./... -race
 
 echo "Running application"
-go run --tags=debug main.go binassets.go
+go run --tags=debug main.go

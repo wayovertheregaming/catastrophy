125d6932f31615f914e43aecd4d805816c7d3d2f
Ben Cragg <bcvery1@gmail.com>
2018-12-02 12:29:23 +0000

    Initial commit

---
+++ levels/shadowRealm.go
@@ -0,0 +1,69 @@
+package levels
+
+import (
+	"image"
+
+	"github.com/faiface/pixel"
+	"github.com/faiface/pixel/pixelgl"
+	"github.com/wayovertheregaming/catastrophy/catlog"
+	"github.com/wayovertheregaming/catastrophy/player"
+	"github.com/wayovertheregaming/catastrophy/util"
+)
+
+const (
+	shadowImagePath = "assets/graphics/shadowRealm.png"
+)
+
+var (
+	// ShadowRealm is the ground level
+	ShadowRealm = &Level{
+		name:          "Shadow Realm",
+		updateFunc:    updateShadow,
+		drawFunc:      drawShadow,
+		initFunc:      initShadow,
+		displayPlayer: true,
+	}
+
+	shadowBackgroundSprite *pixel.Sprite
+	shadowBackgroundPic    pixel.Picture
+
+	// shadowImageDimensions is effectively the size of the image
+	shadowImageDimensions pixel.Rect
+	shadowFloorStartPos   = pixel.V(500, 200)
+)
+
+func init() {
+	catlog.Debug("Preparing shadow realm level")
+
+	// Load the background image
+	shadowBackgroundSprite, shadowBackgroundPic = util.LoadSprite(shadowImagePath, shadowImageDimensions)
+
+	// shadowImageConfig returns dimensions of groundImagePath
+	shadowImageConfig, _, err := image.DecodeConfig(util.GetReaderFromFile(groundImagePath))
+	if err != nil {
+		catlog.Fatalf("Could not load shadow realm image %v", err)
+	}
+
+	// shadowmageDimensions is effectively the size of the image
+	shadowImageDimensions = pixel.R(0, 0, float64(shadowImageConfig.Width), float64(shadowImageConfig.Height))
+
+	// Load the background image
+	shadowBackgroundSprite, shadowBackgroundPic = util.LoadSprite(shadowImagePath, shadowImageDimensions)
+}
+
+func initShadow() {
+	player.SetPos(shadowFloorStartPos)
+}
+
+func updateShadow(dt float64, win *pixelgl.Window) {
+	// Try move the player
+	if !movePlayer(win, dt, groundFloorCollisions) {
+		// Player is not moving, update animation
+		player.AnimateIdle()
+	}
+}
+
+func drawShadow(target pixel.Target) {
+	// inverseMoved is the player position inversed
+	shadowBackgroundSprite.Draw(target, pixel.IM)
+}

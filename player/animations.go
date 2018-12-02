96581f0671b004028f9f08ff62533564d98e141e
Ben Cragg <bcvery1@gmail.com>
2018-12-02 10:11:24 +0000

    Added sprite getting functions

--- player/animations.go
+++ player/animations.go
@@ -2,6 +2,7 @@ package player
 
 import (
 	"github.com/faiface/pixel"
+	"github.com/wayovertheregaming/catastrophy/catlog"
 	"github.com/wayovertheregaming/catastrophy/util"
 )
 
@@ -31,16 +32,60 @@ var (
 )
 
 var (
-	sleepingSprites = make(map[*pixel.Sprite]pixel.Picture)
-	idlingSprites   = make(map[*pixel.Sprite]pixel.Picture)
-	sittingSprites  = make(map[*pixel.Sprite]pixel.Picture)
-	walkingSprites  = make(map[*pixel.Sprite]pixel.Picture)
+	sleepingSprites = []spritePic{}
+	idlingSprites   = []spritePic{}
+	sittingSprites  = []spritePic{}
+	walkingSprites  = []spritePic{}
 )
 
+type spritePic struct {
+	sprite *pixel.Sprite
+	pic    *pixel.Picture
+}
+
 func init() {
+	sleepingSprites = loadIntoVar(sleeping)
+	idlingSprites = loadIntoVar(idling)
+	sittingSprites = loadIntoVar(sitting)
+	walkingSprites = loadIntoVar(walking)
+}
+
+func loadIntoVar(assetList map[string]pixel.Rect) []spritePic {
+	retSlice := []spritePic{}
+
 	// Get the sprites from the image name list
-	for imgPath, r := range sleeping {
+	for imgPath, r := range assetList {
+		catlog.Debugf("Loading player animation sprite %s", imgPath)
+
 		s, p := util.LoadSprite(imgPath, r)
-		sleepingSprites[s] = p
+		sp := spritePic{s, &p}
+		retSlice = append(retSlice, sp)
+	}
+
+	return retSlice
+}
+
+// getAnimationFrame returns the sprite/picture of the sprite to display
+func getAnimationFrame() {
+
+}
+
+func stateFrameToSprites(state, frame int) spritePic {
+	return stateToSprites(state)[frame]
+}
+
+func stateToSprites(state int) []spritePic {
+	switch state {
+	case animationStateSleep:
+		return sleepingSprites
+	case animationStateIdle:
+		return idlingSprites
+	case animationStateSitting:
+		return sittingSprites
+	case animationStateWalking:
+		return walkingSprites
+	default:
+		catlog.Infof("Could not find animation state with index %d, returning idle", state)
+		return idlingSprites
 	}
 }

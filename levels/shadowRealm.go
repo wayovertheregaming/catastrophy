7bc6c39fa85c27fa305ff83dec2a1698c11e32da
Ben Cragg <bcvery1@gmail.com>
2018-12-03 15:13:45 +0000

    Started cat god bobbing

--- levels/shadowRealm.go
+++ levels/shadowRealm.go
@@ -2,6 +2,7 @@ package levels
 
 import (
 	"image"
+	"math"
 
 	"github.com/faiface/pixel"
 	"github.com/faiface/pixel/pixelgl"
@@ -13,6 +14,7 @@ import (
 
 const (
 	shadowImagePath = "assets/graphics/shadowRealm.png"
+	catGodImagePath = "assets/graphics/catGod.png"
 )
 
 var (
@@ -30,7 +32,12 @@ var (
 
 	// shadowImageDimensions is effectively the size of the image
 	shadowImageDimensions pixel.Rect
-	shadowFloorStartPos   = pixel.V(500, 200)
+	shadowFloorStartPos   = pixel.V(40, -1000)
+
+	catGodSprite       *pixel.Sprite
+	catGodPos          = pixel.V(-10, 300)
+	catGodShift        = pixel.ZV
+	catGodShiftCounter float64
 )
 
 func init() {
@@ -51,6 +58,15 @@ func init() {
 
 	// Load the background image
 	shadowBackgroundSprite, shadowBackgroundPic = util.LoadSprite(shadowImagePath, shadowImageDimensions)
+
+	// Get cat god
+	catGodImageConfig, _, err := image.DecodeConfig(util.GetReaderFromFile(catGodImagePath))
+	if err != nil {
+		catlog.Fatalf("Could not load cat god image %v", err)
+	}
+
+	catGodImageDimensions := pixel.R(0, 0, float64(catGodImageConfig.Width), float64(catGodImageConfig.Height))
+	catGodSprite, _ = util.LoadSprite(catGodImagePath, catGodImageDimensions)
 }
 
 func initShadow() {
@@ -63,9 +79,18 @@ func updateShadow(dt float64, win *pixelgl.Window) {
 		// Player is not moving, update animation
 		player.AnimateIdle()
 	}
+
+	// Move cat god up and down
+	catGodShiftCounter += dt
+	catGodPos.Y = math.Sin(catGodShiftCounter * 10)
 }
 
 func drawShadow() {
 	// inverseMoved is the player position inversed
 	shadowBackgroundSprite.Draw(consts.GameView, pixel.IM.Moved(shadowImageDimensions.Center()))
+
+	catGodSprite.Draw(
+		consts.GameView,
+		pixel.IM.Moved(shadowImageDimensions.Center().Add(catGodShift)).Scaled(shadowImageDimensions.Center(), 3),
+	)
 }

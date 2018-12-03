34e3d86868d9876dc80ddadf88bc8c04387f4e9e
Ben Cragg <bcvery1@gmail.com>
2018-12-03 09:17:05 +0000

    Added upstairs dialogue

--- dialogue/dialogues.go
+++ dialogue/dialogues.go
@@ -2,3 +2,13 @@ package dialogue
 
 // This file is for storing actual dialogues
 // They should all be exported and no functions should be in this file
+
+var (
+	// GoingUpstairs tells the player they are going up the stairs
+	GoingUpstairs = []Dialogue{
+		Dialogue{
+			IsPlayer: false,
+			Text:     "You are going upstairs",
+		},
+	}
+)
